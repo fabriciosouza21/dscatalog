3d149c83b6fa545984f7ac1193e46da86b5f72c1
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-25 17:26:19 -0300

    created test update and findbyid

--- backend/dscatalog/src/test/java/com/fsm/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/fsm/dscatalog/services/ProductServiceTests.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.times;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,7 +23,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.fsm.dscatalog.dto.CategoryDTO;
+import com.fsm.dscatalog.dto.ProductDTO;
+import com.fsm.dscatalog.entities.Category;
 import com.fsm.dscatalog.entities.Product;
+import com.fsm.dscatalog.repositories.CategoryRepository;
 import com.fsm.dscatalog.repositories.ProductRepository;
 import com.fsm.dscatalog.services.exception.DatabaseException;
 import com.fsm.dscatalog.services.exception.NotFoundException;
@@ -34,8 +40,10 @@ public class ProductServiceTests {
 	private long nonExistingId;
 	private long dependentId;
 	private PageImpl<Product> page;
-	
 	private Product product;
+	private Category category;
+	private ProductDTO productDto;
+	
 	
 	@InjectMocks
 	private ProductService service;
@@ -43,35 +51,82 @@ public class ProductServiceTests {
 	@Mock
 	private ProductRepository repository;
 	
+	@Mock
+	private CategoryRepository categoryRepository;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		dependentId = 4;
 		product = Factory.createProduct();
+		category = Factory.createCategory();
 		page = new PageImpl<>(List.of(product));
+		productDto = Factory.createProductDTO();
 		
 		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
 		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
 		
+		Mockito.when(repository.getOne(existingId)).thenReturn(product);
+		Mockito.when(repository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+		
+		Mockito.when(categoryRepository.getOne(existingId)).thenReturn(category);
+		Mockito.when(categoryRepository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+				
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
-			
+	
 	}
+	
 	
 	@Test
 	public void findAllPagedShouldReturnPage() {
 		PageRequest pageRequest = PageRequest.of(0, 5);
 		
-		Page<Product> page = repository.findAll(pageRequest);
+		Page<ProductDTO> page = service.findAll(pageRequest);
 		
 		Assertions.assertNotNull(page);
 		Mockito.verify(repository).findAll(pageRequest);
 	}
 	
+	@Test
+	public void findByIdShouldReturnProductDtoWhenIdExists() {
+		
+		ProductDTO productDTO = service.findById(existingId);
+		
+		Assertions.assertNotNull(productDTO);
+		Mockito.verify(repository).findById(existingId);
+	}
+	
+	@Test
+	public void findByIdShouldThrowNotFoundExceptionWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(NotFoundException.class,()->{
+			service.findById(nonExistingId);
+		});
+		Mockito.verify(repository).findById(nonExistingId);
+	}
+	
+	
+	@Test
+	public void updateShouldReturnProductDtoWhenIdExists() {
+		
+		ProductDTO product = service.update(existingId,productDto);
+		
+		Assertions.assertNotNull(product);
+	}
+	
+	@Test
+	public void updateShouldThrowNotFoundExceptionWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(NotFoundException.class,()->{
+			service.update(nonExistingId,productDto);
+		});
+
+	}
 	@Test
 	public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
 		
